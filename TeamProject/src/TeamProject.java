d1742fda5ae56a8bc5ac632273183eff815656b8
Jessica <Jessica@LAPTOP-HJCP3IKH>
2019-11-20 15:37:07 -0700

    Added some basic gui.

--- TeamProject/src/TeamProject.java
+++ TeamProject/src/TeamProject.java
@@ -1,3 +1,89 @@
-public class TeamProject {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
 
+public class TeamProject extends Application
+{
+	public void start(Stage stage)
+	{
+		//input label, button, textfield, and error messages
+		BorderPane inputPane = new BorderPane();
+		
+		Label inputLabel = new Label("Input file name:");
+		inputPane.setTop(inputLabel);
+		inputPane.setMargin(inputLabel, new Insets(10, 10, 0, 10));
+		
+		TextField inputText = new TextField();
+		inputPane.setLeft(inputText);
+		inputPane.setMargin(inputText, new Insets(10, 10, 10, 10));
+		
+		Button inputButton = new Button("Format");
+		inputPane.setRight(inputButton);
+		inputButton.setMinWidth(60);
+		inputPane.setMargin(inputButton, new Insets(10, 10, 10, 0));
+		
+		Label inputError = new Label("error messages for input");
+		inputError.setTextFill(Color.RED);
+		inputPane.setBottom(inputError);
+		inputPane.setMargin(inputError, new Insets(0, 10, 10, 10));
+
+				
+		//output label, button, textfield, and error messages
+		BorderPane outputPane = new BorderPane();
+				
+		Label outputLabel = new Label("Save as:");
+		outputPane.setTop(outputLabel);
+		outputPane.setMargin(outputLabel, new Insets(10, 10, 0, 10));
+				
+		TextField outputText = new TextField();
+		outputPane.setLeft(outputText);
+		outputPane.setMargin(outputText, new Insets(10, 10, 10, 10));
+				
+		Button outputButton = new Button("Save");
+		outputButton.setMinWidth(60);
+		outputPane.setRight(outputButton);
+		outputPane.setMargin(outputButton, new Insets(10, 10, 10, 0));
+				
+		Label outputError = new Label("error messages for output");
+		outputError.setTextFill(Color.RED);
+		outputPane.setBottom(outputError);
+		outputPane.setMargin(outputError, new Insets(0, 10, 400, 10));
+		
+		//input and output panes
+		BorderPane leftPane = new BorderPane();
+		leftPane.setTop(inputPane);
+		leftPane.setBottom(outputPane);
+		
+		//pane for preview
+		BorderPane rightPane = new BorderPane();
+		
+		Label previewLabel = new Label("Preview:");
+		rightPane.setTop(previewLabel);
+		
+		TextArea preview = new TextArea("the preview");
+		preview.setEditable(false);
+		rightPane.setCenter(preview);
+		
+		//pane for everything
+		BorderPane pane = new BorderPane();
+		pane.setLeft(leftPane);
+		pane.setRight(rightPane);
+		
+		Scene scene = new Scene(pane, 700, 500);
+
+		stage.setTitle("TeamProject");
+		stage.setScene(scene);
+		stage.show();
+	}
+	public static void main(String[] args)
+	{
+		Application.launch(args);  
+	}
 }
