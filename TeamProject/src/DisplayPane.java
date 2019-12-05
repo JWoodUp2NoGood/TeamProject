73acd4d1fe1a9d28f3c43e29cc96639069e62b99
Jessica <Jessica@LAPTOP-HJCP3IKH>
2019-12-05 15:01:33 -0700

    Added handling; read file; started formatting input

---
+++ TeamProject/src/DisplayPane.java
@@ -0,0 +1,186 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
+import java.io.*;
+import java.util.Scanner;
+
+public class DisplayPane extends BorderPane
+{		
+	private BorderPane inputPane, outputPane, leftPane, rightPane;
+	private Label inputLabel, inputError, outputLabel, outputError, previewLabel;
+	private TextField inputText, outputText;
+	private Button inputButton, outputButton;
+	private TextArea preview;
+	
+	public DisplayPane()
+	{
+		//input label, button, textfield, and error messages
+		inputPane = new BorderPane();
+			
+		inputLabel = new Label("Input file name:");
+		inputPane.setTop(inputLabel);
+		inputPane.setMargin(inputLabel, new Insets(10, 10, 0, 10));
+			
+		inputText = new TextField();
+		inputPane.setLeft(inputText);
+		inputPane.setMargin(inputText, new Insets(10, 10, 10, 10));
+			
+		inputButton = new Button("Format");
+		inputPane.setRight(inputButton);
+		inputButton.setMinWidth(60);
+		inputPane.setMargin(inputButton, new Insets(10, 10, 10, 0));
+			
+		inputError = new Label(" ");
+		inputError.setTextFill(Color.RED);
+		inputPane.setBottom(inputError);
+		inputPane.setMargin(inputError, new Insets(0, 10, 10, 10));
+
+					
+		//output label, button, textfield, and error messages
+		outputPane = new BorderPane();
+				
+		outputLabel = new Label("Save as:");
+		outputPane.setTop(outputLabel);
+		outputPane.setMargin(outputLabel, new Insets(10, 10, 0, 10));
+					
+		outputText = new TextField();
+		outputPane.setLeft(outputText);
+		outputPane.setMargin(outputText, new Insets(10, 10, 10, 10));
+					
+		outputButton = new Button("Save");
+		outputButton.setMinWidth(60);
+		outputPane.setRight(outputButton);
+		outputPane.setMargin(outputButton, new Insets(10, 10, 10, 0));
+					
+		outputError = new Label(" ");
+		outputError.setTextFill(Color.RED);
+		outputPane.setBottom(outputError);
+		outputPane.setMargin(outputError, new Insets(0, 10, 400, 10));
+			
+		//input and output panes
+		leftPane = new BorderPane();
+		leftPane.setTop(inputPane);
+		leftPane.setBottom(outputPane);
+			
+		//pane for preview
+		rightPane = new BorderPane();
+			
+		previewLabel = new Label("Preview:");
+		rightPane.setTop(previewLabel);
+		rightPane.setMargin(previewLabel, new Insets(10, 0, 10, 0));
+			
+		preview = new TextArea("");
+		preview.setEditable(false);
+		preview.setMinWidth(520);
+		rightPane.setCenter(preview);
+		rightPane.setMargin(preview, new Insets(0, 20, 20, 0));
+			
+		setLeft(leftPane);
+		setRight(rightPane);
+		
+		//for event handling:
+		inputButton.setOnAction(new Handler());
+		outputButton.setOnAction(new Handler());
+	}
+	
+	private class Handler implements EventHandler<ActionEvent>
+	{
+		public void handle(ActionEvent e)
+		{
+			Object source = e.getSource();
+			
+			
+			if(source == inputButton) //if the format button is pressed
+			{
+				String output = "", line;
+				String justification; //left, right, center, or title
+				String spacing; //single or double
+				String indent; //none, indent, or block
+				String columns; //one or two
+				int characters; //number of characters on the line
+				
+				//try to read the file
+				try
+				{
+					File inputFile = new File(inputText.getText());
+					Scanner input = new Scanner(inputFile);
+					inputError.setText("");
+					justification = "left";
+					spacing = "single";
+					indent = "none";
+					columns = "one";
+					characters = 80;
+					
+					//for the entire input file:
+					while(input.hasNext())
+					{
+						//check for new flags
+						line = input.nextLine();
+						if(line.equals("-l")) { justification = "left"; }						
+						else if(line.equals("-r")) { justification = "right"; }
+						else if(line.equals("-c")) { justification = "center"; }
+						else if(line.equals("-t")) { justification = "title"; }
+						else if(line.equals("-s")) { spacing = "single"; }
+						else if(line.equals("-d")) { spacing = "double"; }
+						else if(line.equals("-i")) { indent = "indent"; }
+						else if(line.equals("-b")) { indent = "block"; }
+						else if(line.equals("-n")) { indent = "none"; }
+						else if(line.equals("-1")) { columns = "one"; }
+						else if(line.equals("-2")) { columns = "two"; }
+						else if(line.equals("-e"))
+						{
+							output = output + "\n";
+						} else {
+							//format the text
+							if(justification.equals("left"))
+							{
+							
+							} else if(justification.equals("right")) {
+							
+							} else if(justification.equals("center")) {
+							
+							} else if(justification.equals("title")) {
+								characters = 80 - line.length();
+							
+								for(int i = 0; i < characters / 2; i++)
+								{
+									line = " " + line;
+								}
+								characters = characters - (characters / 2);
+								for(int i = 0; i < characters; i++)
+								{
+									line = line + " ";
+								}
+								output = output + line + "\n";
+								
+								if(spacing.equals("double"))
+								{
+									output = output + "\n";
+								}
+							}				
+						}
+					}
+					
+					preview.setText(output);
+					input.close();
+				}
+				//if the file is not found, display the error message
+				catch(FileNotFoundException exception)
+				{
+					inputError.setText("File not found");
+				}
+			} else if(source == outputButton) { //if the save button is pressed
+				
+			}
+			
+			
+		}
+	}
+}
--- TeamProject/src/TeamProject.java
+++ TeamProject/src/TeamProject.java
@@ -1,83 +1,15 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.geometry.Insets;
 
 public class TeamProject extends Application
 {
 	public void start(Stage stage)
 	{
-		//input label, button, textfield, and error messages
-		BorderPane inputPane = new BorderPane();
-		
-		Label inputLabel = new Label("Input file name:");
-		inputPane.setTop(inputLabel);
-		inputPane.setMargin(inputLabel, new Insets(10, 10, 0, 10));
-		
-		TextField inputText = new TextField();
-		inputPane.setLeft(inputText);
-		inputPane.setMargin(inputText, new Insets(10, 10, 10, 10));
-		
-		Button inputButton = new Button("Format");
-		inputPane.setRight(inputButton);
-		inputButton.setMinWidth(60);
-		inputPane.setMargin(inputButton, new Insets(10, 10, 10, 0));
-		
-		Label inputError = new Label("error messages for input");
-		inputError.setTextFill(Color.RED);
-		inputPane.setBottom(inputError);
-		inputPane.setMargin(inputError, new Insets(0, 10, 10, 10));
-
-				
-		//output label, button, textfield, and error messages
-		BorderPane outputPane = new BorderPane();
-				
-		Label outputLabel = new Label("Save as:");
-		outputPane.setTop(outputLabel);
-		outputPane.setMargin(outputLabel, new Insets(10, 10, 0, 10));
-				
-		TextField outputText = new TextField();
-		outputPane.setLeft(outputText);
-		outputPane.setMargin(outputText, new Insets(10, 10, 10, 10));
-				
-		Button outputButton = new Button("Save");
-		outputButton.setMinWidth(60);
-		outputPane.setRight(outputButton);
-		outputPane.setMargin(outputButton, new Insets(10, 10, 10, 0));
-				
-		Label outputError = new Label("error messages for output");
-		outputError.setTextFill(Color.RED);
-		outputPane.setBottom(outputError);
-		outputPane.setMargin(outputError, new Insets(0, 10, 400, 10));
-		
-		//input and output panes
-		BorderPane leftPane = new BorderPane();
-		leftPane.setTop(inputPane);
-		leftPane.setBottom(outputPane);
-		
-		//pane for preview
-		BorderPane rightPane = new BorderPane();
-		
-		Label previewLabel = new Label("Preview:");
-		rightPane.setTop(previewLabel);
-		rightPane.setMargin(previewLabel, new Insets(10, 0, 10, 0));
-		
-		TextArea preview = new TextArea("123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234567890");
-		preview.setEditable(false);
-		preview.setMinWidth(520);
-		rightPane.setCenter(preview);
-		rightPane.setMargin(preview, new Insets(0, 20, 20, 0));
-		
-		//pane for everything
+		DisplayPane dispPane = new DisplayPane();
 		BorderPane pane = new BorderPane();
-		pane.setLeft(leftPane);
-		pane.setRight(rightPane);
+		pane.setCenter(dispPane);
 		
 		Scene scene = new Scene(pane, 900, 600);
 
